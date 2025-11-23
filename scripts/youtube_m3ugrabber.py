3bed235815c64cf2da537ccfa56e15dda0d493d3
longzhi456 <slw204701@gmail.com>
2025-11-23 21:43:24 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -13,26 +13,35 @@ windows = False
 if 'win' in sys.platform:
     windows = True
 
-def grab(youtube_url, timeout=15):
-    try:
-        # 运行 yt-dlp 命令
-        result = subprocess.run(
-            ['yt-dlp', '-f', 'best', '--get-url', youtube_url], 
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-        )
-        
-        # 获取并返回结果
-        m3u8_url = result.stdout.strip()  # 去除输出中的多余空白字符
-        if m3u8_url:
-            print(m3u8_url)
-            return
+def grab(url):
+    response = requests.get(url, timeout=15).text
+    if '.m3u8' not in response:
+        #response = requests.get(url).text
+        if '.m3u8' not in response:
+            if windows:
+                print('https://xxxx.m3u')
+                return
+            #os.system(f'wget {url} -O temp.txt')
+            os.system(f'curl "{url}" > temp.txt')
+            response = ''.join(open('temp.txt').readlines())
+            if '.m3u8' not in response:
+                print('https://xxxx.m3u')
+                return
+    end = response.find('.m3u8') + 5
+    tuner = 100
+    while True:
+        if 'https://' in response[end-tuner : end]:
+            link = response[end-tuner : end]
+            start = link.find('https://')
+            end = link.find('.m3u8') + 5
+            break
         else:
-            print("https://xxxx.m3u8")
-            return None
-    except Exception as e:
-        print("https://xxxx.m3u8")
-        return None
-         
+            tuner += 5
+    print(f"{link[start : end]}")
+
+    print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
+
+#s = requests.Session()
 with open('../youtube_channel_info.txt') as f:
     for line in f:
         line = line.strip()
