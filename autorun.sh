da13d71425d4f1aba9b904f53ec39aad3e2dcfcb
longzhi456 <slw204701@gmail.com>
2025-11-23 15:02:21 +0800

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -3,9 +3,8 @@
 echo $(dirname $0)
 
 python3 -m pip install requests
+python3 -m pip install pandas
 
 cd $(dirname $0)/scripts/
 
-python3 getUrls.py > ../result.m3u
-
-echo m3u grabbed
+python3 youtube_to_m3u.py
