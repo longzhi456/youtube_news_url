0d2222e8594ed0f3685471128784d3765bb1d561
longzhi456 <slw204701@gmail.com>
2026-03-08 17:06:47 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -3,4 +3,11 @@
 import requests
 
 with open('../youtube_channel_info.txt') as f:
-    print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+        if line.startswith('#EXTINF:-1'):
+            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+        else
+            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na1.m3u')
