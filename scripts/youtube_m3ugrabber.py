e4a0f821d0c8cdaf979c292882a98b37608a2bef
longzhi456 <slw204701@gmail.com>
2026-03-08 17:13:21 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -5,7 +5,9 @@ import requests
 with open('../youtube_channel_info.txt') as f:
     for line in f:
         line = line.strip()
-
+        if not line:
+            print('https://xxx.m3u')
+            return
         if line.startswith('#EXTINF:-1'):
             print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
         else
