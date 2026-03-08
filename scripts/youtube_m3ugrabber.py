775b030259203aaeec2c82f5ea067ecb960e821b
longzhi456 <slw204701@gmail.com>
2026-03-08 15:46:54 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -27,12 +27,4 @@ def get_live_url(channel_id: str) -> str | None:
     else:
         print("No live stream")  
 
-with open('../youtube_channel_info.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line:
-            continue
-        if line.startswith('#EXTINF:'):
-            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
-        else:
-            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_n2a.m3u')
+print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_n2a.m3u')
