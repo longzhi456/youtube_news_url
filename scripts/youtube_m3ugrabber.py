7282d32cfc86e12be590e217ee7696178dad7253
longzhi456 <slw204701@gmail.com>
2026-03-08 15:51:47 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -2,29 +2,4 @@
 
 import requests
 
-def get_live_url(channel_id: str) -> str | None:
-    url = f"https://www.youtube.com/channel/{channel_id}/live"
-        
-    try:
-        r = requests.get(url, allow_redirects=True, timeout=10)
-
-        if "watch?v=" in r.url:
-            return r.url
-
-        return None
-
-    except Exception as e:
-        print("error:", e)
-        return None
-
-    if __name__ == "__main__":
-    channel_id = "UC5nwNW4KdC0SzrhF9BXEYOQ"
-
-    live_url = get_live_url(channel_id)
-
-    if live_url:
-        print("LIVE URL:", live_url)
-    else:
-        print("No live stream")  
-
 print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_n2a.m3u')
