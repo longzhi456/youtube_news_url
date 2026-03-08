fd8ac759dcf45bfabe4c8a261e7c0a2ab72af675
longzhi456 <slw204701@gmail.com>
2026-03-08 17:18:47 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -4,8 +4,9 @@ import requests
 
 with open('../youtube_channel_info.txt') as f:
     print('https://111.m3u')
-    #for line in f:
-        #line = line.strip()
+    for line in f:
+        line = line.strip()
+        print(f'\nhttps://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na1.m3u')
         #if not line:
            # print(f'\nhttps://xxx.m3u')
             #return
