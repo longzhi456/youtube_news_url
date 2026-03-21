689a2d97a3e9caef2b4de88d05d1fa95127d5e5e
longzhi456 <slw204701@gmail.com>
2026-03-21 15:16:08 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -2,6 +2,12 @@
 
 import requests
 
+def grab(ch_id):
+    if not ch_id:
+         print('https://xxxx.m3u')
+    else:
+        print('https://yyyy.m3u')
+
 with open('../youtube_channel_info.txt') as f:
     print('#EXTM3U x-tvg-url="https://live.fanmingming.cn/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"')
     for line in f:
