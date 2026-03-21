1a5298ec1efd42eb221876b52e8e715fd2b611db
longzhi456 <slw204701@gmail.com>
2026-03-21 15:28:53 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -4,20 +4,20 @@ import requests
 
 def grab(ch_id):
     if not ch_id:
-         print('https://xxxx.m3u')
-    else:
-        url = f"https://www.youtube.com/channel/{ch_id}/live"
-        try:
-            r = requests.get(url, allow_redirects=True, timeout=10)
-            if "watch?v=" in r.url:
-                print(r.url)
-                return
+        print('https://xxxx.m3u')
+        return
 
-            print('https://xxxx.m3u')
+    url = f"https://www.youtube.com/channel/{ch_id}/live"
+    try:
+        r = requests.get(url, allow_redirects=True, timeout=10)
+        if "watch?v=" in r.url:
+            print(r.url)
             return
+
+        print('https://xxxx.m3u')
+
     except Exception as e:
         print('https://xxxx.m3u')
-        return None
 
 with open('../youtube_channel_info.txt') as f:
     print('#EXTM3U x-tvg-url="https://live.fanmingming.cn/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"')
